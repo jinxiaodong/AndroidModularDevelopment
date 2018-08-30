05daf2f0d5626ab272d29359f6a3ba7a829db82d
jinxiaodong <xiaodong.jin@chinaredstar.com>
2018-08-30 18:13:47 +0800

    gradle 统一配置

---
+++ login/src/main/java/debug/ModuleApplication.java
@@ -0,0 +1,12 @@
+package debug;
+
+
+import android.app.Application;
+
+public class ModuleApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ login/src/main/java/debug/MoudleActivity.java
@@ -0,0 +1,25 @@
+package debug;
+
+import android.app.Activity;
+
+public class MoudleActivity extends Activity{
+
+//    @Override
+//    public int bindLayout() {
+//        return R.layout.activitymodule_test;
+//    }
+//
+//    @Override
+//    public void initView(Activity activity) {
+//
+//    }
+//
+//    @Override
+//    public void initData(Context context) {
+//
+//    }
+//
+//    public void click(View view) {
+//        RouterUtil.navigation(this, ARoutePath.login.SmsLoginActivity);
+//    }
+}
